e2b1dc7feb120fcfb845f10d92e914ef725064f7
Marin Mema <mmema@uwo.ca>
2017-05-24 22:07:10 -0700

    Added server main loop function skeleton.

--- dcandy.c
+++ dcandy.c
@@ -1,6 +1,42 @@
 /* dcandy.c */
 
+typedef unsigned char   b1_t;
+typedef unsigned short  b2_t;
+typedef unsigned int    b4_t;
+typedef unsigned double b8_t;
+
+#define SUCCESS 0
+#define FAILURE 1
+
+// See fcserver.cpp/FCServer::mainLoop()
+void main_loop();
+
+void main_loop()
+{
+	// Indefinite loop
+	for (;;)
+	{
+		// Loop through connected USB devices and flush their communication 
+		// streams.
+		b4_t i = 0;
+		b4_t n = 0;
+
+		for (i = 0; i < n; i++)
+		{
+
+		}
+	}
+	return;
+}
+
 int main()
 {
+	// Initialize libusb context/struct
+	
+	// Initialize and start server object
+	
+	// Server main loop
+	main_loop();
+
 	return 0;
 }
